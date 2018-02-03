2b515abdcf0986f87ae75baba89370cbaa9a0576
Mike Christensen <netadptr0719@hotmail.com>
2018-02-03 16:59:42 -0600

    Moved to using Mongo

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var jobs = require('./jobs');
-var skills = require('./skills');
+//var jobs = require('./jobs');
+//var skills = require('./skills');
+var mongodb = require('mongodb').MongoClient;
 
 app.use(express.static('public'));
 app.use('/resume', express.static('public'));
@@ -11,7 +12,18 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('/resume', function(req,res){
-    res.render('index', {jobs: jobs, skills: skills});
+    mongodb.connect("mongodb://localhost:27017/resume", function(err, db){
+        if (err) { return console.dir(err); }
+        else {
+            var jobColl = db.collection("jobs");
+            var skillColl = db.collection("skills");
+            jobColl.find({}).toArray(function(err, jobs){
+                skillColl.find({}).toArray(function(err, skills){
+                    res.render('index', {jobs: jobs, skills: skills});
+                });
+            });
+        }
+    });
 });
 
 app.listen(3000);
